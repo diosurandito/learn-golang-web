cb4e675d75668e7b0b89694796a546bae32e9185
diosurandito <diosurandito@gmail.com>
2021-12-08 11:32:04 +0700

    11.Header

---
+++ header_test.go
@@ -0,0 +1,52 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func RequestHeader(writer http.ResponseWriter, request *http.Request) {
+	contentType := request.Header.Get("content-type")
+	fmt.Fprint(writer, contentType)
+}
+
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/", nil)
+	request.Header.Add("Content-Type", "application/json")
+	recorder := httptest.NewRecorder()
+
+	RequestHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+	assert.Equal(t, "application/json", string(body), "They Shoul be equal")
+}
+
+func ResponseHeader(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("X-Powered-By", "KJX Code")
+	fmt.Fprint(writer, "OK")
+}
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost:8080", nil)
+	request.Header.Add("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+
+	ResponseHeader(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(string(body))
+	xPoweredBy := response.Header.Get("x-powered-by")
+	fmt.Println(xPoweredBy)
+	assert.Equal(t, "KJX Code", xPoweredBy, "They Shoul be equal")
+
+}
