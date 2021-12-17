33ae753c873a0e3f9c2ee8284de08ac79c2c7793
diosurandito <diosurandito@gmail.com>
2021-12-17 10:05:50 +0700

    13.Response Code

---
+++ response_code_test.go
@@ -0,0 +1,53 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func ResponseCode(writer http.ResponseWriter, request *http.Request) {
+	name := request.URL.Query().Get("name")
+	if name == "" {
+		writer.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(writer, "name is empty")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", name)
+	}
+}
+
+func TestResponseCodeInvalid(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+	assert.Equal(t, "name is empty", string(body), "They Shoul be equal")
+
+}
+
+func TestResponseCodeValid(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080/?name=Dio", nil)
+	recorder := httptest.NewRecorder()
+
+	ResponseCode(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+	assert.Equal(t, "Hello Dio", string(body), "They Shoul be equal")
+
+}
